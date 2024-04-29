f26c310cdb24372e9e3da08a68325752663e476c
“Abid” <abdur.abid@madetech.com>
2024-04-29 16:46:23 +0100

    add dockerfile and modify app.py for deployment

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route("/")
 def home():
     TOKEN = os.getenv("EPC_ENCODED_API_TOKEN")
     # QUERY_PARAMS = {"uprn" : "200002791"}
-    QUERY_PARAMS = {'local-authority' :'E09000008', 'size' : '2'}
+    QUERY_PARAMS = {"local-authority": "E09000008", "size": "2"}
     HEADERS = {"Accept": "application/json", "Authorization": f"Basic {TOKEN}"}
-    array_of_properties = epc_api_call(HEADERS, {'local-authority' :'E09000008', 'size' : '50'})['rows']
+    array_of_properties = epc_api_call(
+        HEADERS, {"local-authority": "E09000008", "size": "50"}
+    )["rows"]
     props = get_props(array_of_properties)
-    return render_template('home.html', props=props)
+    return render_template("home.html", props=props)
+
 
 # Define other routes as needed
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
