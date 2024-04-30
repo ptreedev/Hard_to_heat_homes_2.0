e665b86d415aba1e2cfb0959c467b33dbaee9949
“Abid” <abdur.abid@madetech.com>
2024-04-30 15:51:44 +0100

    test: GREEN, os_api_call() => type == dict

--- tests/test_os_api.py
+++ tests/test_os_api.py
@@ -1,5 +1,25 @@
-def os_api_call(headers):
-    return False
+import urllib.request
+from urllib.parse import urlencode
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+HEADERS = {"Accept": "application/json"}
+BASE_URL = 'https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-building-2/items?'
+OS_API_KEY = os.getenv('OS_API_KEY')
+
+def os_api_call(headers, params):
+    full_url = f'{BASE_URL}{urlencode(params)}'
+    try: 
+        with urllib.request.urlopen(urllib.request.Request(full_url, headers=headers)) as response:
+            return {}
+    except Exception:
+        return False
 
 def test_not_200_response():
-    assert os_api_call({}) == False
+    assert os_api_call({}, {}) == False
+
+def test_200_response():
+    assert type(os_api_call(HEADERS, {'key': OS_API_KEY})) is dict
