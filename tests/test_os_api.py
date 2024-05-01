2b594c75def4fcd5bec2dfb7ce08066a06af5884
“Abid” <abdur.abid@madetech.com>
2024-05-01 12:45:31 +0100

    REFACTOR change of dummy data to include multiple properties and uprns

--- tests/test_os_api.py
+++ tests/test_os_api.py
@@ -31,11 +31,12 @@ response = os_api_call(
     HEADERS,
     {
         "key": OS_API_KEY,
-        "limit": 1,
+        "limit": 3,
         "filter": "oslandusetiera LIKE 'Residential Accommodation' AND ismainbuilding=true",
+        "bbox": "-0.372438,51.405655,-0.371885,51.40600",
     },
 )
-# https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-building-2/items?key=eQZohxBxjpHhrXsRrcVCxu5TI1XFObsw&limit=1&filter=oslandusetiera%20LIKE%20'Residential Accommodation'%20AND%20ismainbuilding=true
+# https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-building-2/items?key=eQZohxBxjpHhrXsRrcVCxu5TI1XFObsw&filter=oslandusetiera%20LIKE%20'Residential Accommodation'%20AND%20ismainbuilding=true&bbox=-0.372438,51.405655,-0.371885,51.40600&limit=3
 
 
 def test_not_200_response():
@@ -51,4 +52,5 @@ def test_features_array_is_populated():
 
 
 def test_accessing_desired_response_data():
-    assert response["features"][0]["properties"]["uprnreference"][0]["uprn"] == os_dummy_data["features"][0]["properties"]["uprnreference"][0]["uprn"]
+    for i in range(len(response["features"][0]["properties"]["uprnreference"])):
+        assert response["features"][0]["properties"]["uprnreference"][i]["uprn"] == os_dummy_data["features"][0]["properties"]["uprnreference"][i]["uprn"]
