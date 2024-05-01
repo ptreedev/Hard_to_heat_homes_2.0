af6fba30f0a18c6fe952dd7369ae702c3c8ae2c5
“Abid” <abdur.abid@madetech.com>
2024-05-01 11:26:29 +0100

    test: GREEN response features array is len > 0

--- tests/test_os_api.py
+++ tests/test_os_api.py
@@ -8,21 +8,31 @@ load_dotenv()
 
 
 HEADERS = {"Accept": "application/json"}
-BASE_URL = 'https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-building-2/items?'
-OS_API_KEY = os.getenv('OS_API_KEY')
+BASE_URL = "https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-building-2/items?"
+OS_API_KEY = os.getenv("OS_API_KEY")
+
 
 def os_api_call(headers, params):
-    full_url = f'{BASE_URL}{urlencode(params)}'
-    try: 
-        with urllib.request.urlopen(urllib.request.Request(full_url, headers=headers)) as response:
+    full_url = f"{BASE_URL}{urlencode(params)}"
+    try:
+        with urllib.request.urlopen(
+            urllib.request.Request(full_url, headers=headers)
+        ) as response:
             response_body = response.read()
             return json.loads(response_body)
-           
+
     except Exception:
         return False
+
 
 def test_not_200_response():
     assert os_api_call({}, {}) == False
 
+
 def test_200_response():
-    assert type(os_api_call(HEADERS, {'key': OS_API_KEY})) is dict
+    assert type(os_api_call(HEADERS, {"key": OS_API_KEY})) is dict
+
+
+def test_features_array_is_populated():
+    response = os_api_call(HEADERS, {"key": OS_API_KEY})
+    assert len(response["features"]) > 0
