3b5ce5334ff14878a4eb943d41c8e50f775b54c4
“Abid” <abdur.abid@madetech.com>
2024-05-02 11:29:44 +0100

    test: GREEN return two buildings from OS api call ,,,,,,

--- tests/test_multiple_properties.py
+++ tests/test_multiple_properties.py
@@ -3,10 +3,16 @@ from src.property import Property
 from dotenv import load_dotenv
 import os 
 from app import get_props
+from tests.test_os_api import os_api_call
+import json
+
+with open("src/os_dummy_data.json") as data:
+    os_dummy_data = json.load(data)
 
 load_dotenv()
 
 TOKEN = os.getenv('EPC_ENCODED_API_TOKEN')
+
 
 BASE_URL = 'https://epc.opendatacommunities.org/api/v1/domestic/search?'
 HEADERS = {
@@ -14,6 +20,13 @@ HEADERS = {
         'Authorization': f'Basic {TOKEN}'
     }
 
+OS_API_KEY = os.getenv("OS_API_KEY")
+OS_PARAMS = {
+        "key": OS_API_KEY,
+        "limit": 2,
+        "filter": "oslandusetiera LIKE 'Residential Accommodation' AND ismainbuilding=true",
+        "bbox": "-0.372438,51.405655,-0.371885,51.40600",
+    }
 
 def test_returns_two_rows_of_properties():
     array_of_properties = epc_api_call(HEADERS, {'local-authority' :'E09000008', 'size' : '2'})['rows']
@@ -24,4 +37,13 @@ def test_returns_an_array_of_two_property_instances():
     props = get_props(array_of_properties)
     assert type(props) is list
     assert type(props[0]) is Property
+
+def test_returns_two_buildings_from_os():
+    response = os_api_call({"Accept":"application/json"}, OS_PARAMS)
+    array_of_buildings = response["features"]
+    assert len(array_of_buildings) > 0
+
+
+
+
     
