dfec8b37e5ff010039846b744b9ac388538701a7
“Abid” <abdur.abid@madetech.com>
2024-05-02 12:10:30 +0100

    REFACTOR - DRY

--- tests/test_multiple_properties.py
+++ tests/test_multiple_properties.py
@@ -31,14 +31,15 @@ OS_PARAMS = {
 
 def get_props_from_os(list_of_buildings):
     result = []
-    for building in range(len(list_of_buildings)):
-        uprn_array = list_of_buildings[building]["properties"]["uprnreference"]
-        for property in range(len(uprn_array)):
-            age = "buildingage_year" if list_of_buildings[building]["properties"]["buildingage_year"] else "buildingage_period"
-            new_prop = Property(uprn_array[property]['uprn'])
-            new_prop.connectivity =  list_of_buildings[building]["properties"]["connectivity"]
-            new_prop.age =  list_of_buildings[building]["properties"][age]
-            new_prop.material =  list_of_buildings[building]["properties"]["constructionmaterial"]
+    for i in range(len(list_of_buildings)):
+        building = list_of_buildings[i]["properties"]
+        uprn_array = building["uprnreference"]
+        for j in range(len(uprn_array)):
+            age = "buildingage_year" if building["buildingage_year"] else "buildingage_period"
+            new_prop = Property(uprn_array[j]['uprn'])
+            new_prop.connectivity =  building["connectivity"]
+            new_prop.age =  building[age]
+            new_prop.material =  building["constructionmaterial"]
             result.append(new_prop)
     return result
 
