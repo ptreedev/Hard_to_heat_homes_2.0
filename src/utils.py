50418614118e7ac88724c53d13456fe53b4a0f73
“Abid” <abdur.abid@madetech.com>
2024-05-02 16:53:48 +0100

    clean up

--- src/utils.py
+++ src/utils.py
@@ -22,25 +22,6 @@ def get_props_from_os(list_of_buildings):
             new_prop.connectivity = building["connectivity"]
             new_prop.age = building[age]
             new_prop.material = building["constructionmaterial"]
-            # epc_result = epc_api_call(
-            #     {"Accept": "application/json", "Authorization": f"Basic {TOKEN}"},
-            #     f'uprn={uprn_array[j]["uprn"]}',
-            # )
-            # new_prop.epc_rating = (
-            #     epc_result["rows"][0]["current-energy-rating"]
-            #     if epc_result
-            #     else "Not found"
-            # )
-            # new_prop.epc_score = (
-            #     epc_result["rows"][0]["current-energy-efficiency"]
-            #     if epc_result
-            #     else "Not found"
-            # )
-            # new_prop.address = (
-            #     f'{epc_result["rows"][0]["address"]}, {epc_result["rows"][0]["postcode"]}'
-            #     if epc_result
-            #     else "Not found"
-            # )
             result.append(new_prop)
 
     return result
@@ -60,10 +41,7 @@ def get_attributes_from_epc(properties):
                 prop.address = (
                     f'{epc_result_rows[j]["address"]}, {epc_result_rows[j]["postcode"]}'
                 )
-            # else:
-            #     prop.epc_rating = "N/A"
-            #     prop.epc_score = "N/A"
-            #     prop.address = "N/A"
+                
 
 def get_urpns_from_properties(properties):
     base_str = "uprn"
