9e8f4e99785691776ce2ebdfa94ab9761846a165
Harriet <harriet.hall@madetech.com>
2024-05-13 13:05:19 +0100

    create page per property route using uprn

--- app.py
+++ app.py
@@ -5,25 +5,35 @@ from src.variables import OS_KEY
 
 app = Flask(__name__)
 
+list_of_buildings = os_api_call({"Accept":"application/json"}, {
+    "key": OS_KEY,
+    "filter": "oslandusetiera LIKE 'Residential Accommodation' AND ismainbuilding=true",
+    "bbox": "-0.373641,51.399234,-0.372031,51.399977",
+})["features"]
+properties = get_properties_from_os(list_of_buildings)
+get_attributes_from_epc(properties)
+setting_void_properties(properties)
+for i in range(len(properties)):
+    # set_missing_addresses(properties[i])
+    properties[i].calculate_score()
+
+
 @app.route("/")
 def home():
-    list_of_buildings = os_api_call({"Accept":"application/json"}, {
-        "key": OS_KEY,
-        "filter": "oslandusetiera LIKE 'Residential Accommodation' AND ismainbuilding=true",
-        "bbox": "-0.373641,51.399234,-0.372031,51.399977",
-    })["features"]
-    props = get_properties_from_os(list_of_buildings)
-    get_attributes_from_epc(props)
-    setting_void_properties(props)
-    for i in range(len(props)):
-        # set_missing_addresses(props[i])
-        props[i].calculate_score()
+   return render_template("home.html", properties=properties, key=OS_KEY)
 
-    dummy_dict = {"uprn" : '1', "address" : "something", "void" : True}
-    return render_template("home.html", props=props, key=OS_KEY, dummy_dict=dummy_dict)
+@app.route("/<int:uprn>")
+def property(uprn):
+    prop = None
+    for property in properties:
+        if property.uprn == uprn:
+            prop = property 
+            # handle error 404
+
+    return render_template("property.html", property=prop)
 
 
-# Define other routes as needed
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
