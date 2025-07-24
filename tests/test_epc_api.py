a467722ff59baba0164e53589de6205b7c4bccf3
Adil Rahman <arahman.99@outlook.com>
2025-07-24 15:12:59 +0100

    Add test via mocking and remove previous tests

--- tests/test_epc_api.py
+++ tests/test_epc_api.py
@@ -4,41 +4,11 @@ from src.variables import EPC_TOKEN
 
 QUERY_PARAMS = "uprn=200002791&uprn=100061342030"
 HEADERS = {"Accept": "application/json", "Authorization": f"Basic {EPC_TOKEN}"}
-response_data = epc_api_call(HEADERS, QUERY_PARAMS)
 
-def test_200_response():
-    assert type(response_data) is dict
-
-
-def test_200_response_invalid_uprn():
-    assert epc_api_call(HEADERS, "uprn=123456789") == {}
-
-
-def test_not_200_response():
-    assert epc_api_call({}, QUERY_PARAMS) == False
-
-
-def test_result_is_JSON():
-    assert response_data == epc_dummy_data
-
-
-def test_dummy_data_address():
-    assert response_data["rows"][0]["address"] == epc_dummy_data["rows"][0]["address"]
-    assert response_data["rows"][0]["address"] == "30 Alexandra Road, Muswell Hill"
-
-
-def test_dummy_data_epc_rating():
-    assert (
-        response_data["rows"][0]["current-energy-rating"]
-        == epc_dummy_data["rows"][0]["current-energy-rating"]
-    )
-    assert response_data["rows"][0]["current-energy-rating"] == "D"
-
-
-def test_dummy_data_epc_score():
-    print(f'{response_data["rows"]}')
-    assert (
-        response_data["rows"][0]["current-energy-efficiency"]
-        == epc_dummy_data["rows"][0]["current-energy-efficiency"]
-    )
-    assert response_data["rows"][0]["current-energy-efficiency"] == "63"
+def test_get_request_is_called_once(mocker):
+    mock_get = mocker.patch("src.epc_api.requests.get")
+    mock_response = mock_get.return_value
+    mock_response.json.return_value = epc_dummy_data
+    result = epc_api_call(HEADERS, QUERY_PARAMS)
+    assert result == epc_dummy_data
+    mock_get.assert_called_once()
