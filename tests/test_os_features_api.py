ac676c46892f5cca89a0f6b7a37b4c787a803ca8
padlichan <lili.berenyi@gmail.com>
2025-07-28 14:41:55 +0100

    Refactor test_os_features_api to mock api call

--- tests/test_os_features_api.py
+++ tests/test_os_features_api.py
@@ -1,37 +1,47 @@
 import json
+import pytest
 from src.os_api import os_api_call
 from src.variables import OS_KEY
 
 with open("tests/test_data/os_dummy_data.json") as data:
     os_dummy_data = json.load(data)
 
+with open("tests/test_data/os_bad_call_dummy_data.json") as data:
+    os_bad_call_dummy_data = json.load(data)
+
 HEADERS = {"Accept": "application/json"}
-
-
-response = os_api_call(
-    HEADERS,
-    {
+PARAMS = {
         "key": OS_KEY,
         "limit": 3,
         "filter": "oslandusetiera LIKE 'Residential Accommodation' AND ismainbuilding=true",
         "bbox": "-0.372438,51.405655,-0.371885,51.40600",
-    },
-)
+         }
+
+@pytest.fixture
+def mock_os_api_response(mocker):
+    mock_response = os_dummy_data
+    mock_get = mocker.patch("src.os_api.requests.get")
+    mock_get.return_value.json.return_value = mock_response
+
+    return os_api_call(HEADERS, PARAMS)
+
+def test_not_200_response(mocker):
+    mock_response = os_bad_call_dummy_data
+    mock_get = mocker.patch("src.os_api.requests.get")
+    mock_get.return_value.json.return_value = mock_response
+    response = os_api_call({}, {})
+    assert response == os_bad_call_dummy_data
 
 
-def test_not_200_response():
-    assert os_api_call({}, {}) == False
+def test_200_response(mock_os_api_response):
+    assert type(mock_os_api_response) is dict
 
 
-def test_200_response():
-    assert type(response) is dict
+def test_features_array_is_populated(mock_os_api_response):
+    assert len(mock_os_api_response["features"]) > 0
 
-
-def test_features_array_is_populated():
-    assert len(response["features"]) > 0
-
-first_building = response["features"][0]["properties"]
-def test_one_building_accessing_desired_response_data():
+def test_one_building_accessing_desired_response_data(mock_os_api_response):
+    first_building = mock_os_api_response["features"][0]["properties"]
     assert first_building["connectivity"] == "Semi-Connected"
     year = (
         first_building["buildingage_year"]
@@ -41,15 +51,16 @@ def test_one_building_accessing_desired_response_data():
     assert year == "1945-1959"
     assert first_building["constructionmaterial"] == "Brick Or Block Or Stone"
 
-def test_get_uprns_from_one_building():
+def test_get_uprns_from_one_building(mock_os_api_response):
+    first_building = mock_os_api_response["features"][0]["properties"]
     for i in range(len(first_building["uprnreference"])):
         assert first_building["uprnreference"][i]['uprn'] == os_dummy_data["features"][0]["properties"]["uprnreference"][i]["uprn"]
 
 
-def test_accessing_desired_response_data_for_multiple_buildings():
-    for i in range(len(response["features"])):
-        response_building = response["features"][i]["properties"]
-        response_building_coordinates = response["features"][i]["geometry"]["coordinates"]
+def test_accessing_desired_response_data_for_multiple_buildings(mock_os_api_response):
+    for i in range(len(mock_os_api_response["features"])):
+        response_building = mock_os_api_response["features"][i]["properties"]
+        response_building_coordinates = mock_os_api_response["features"][i]["geometry"]["coordinates"]
         dummy_data_building = os_dummy_data["features"][i]["properties"]
         
         year = "buildingage_year" if response_building["buildingage_year"] else "buildingage_period"
