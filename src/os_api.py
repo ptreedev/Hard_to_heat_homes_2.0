ed96d4690dd71209c134f2f2487ef3c49be960b8
Adil Rahman <arahman.99@outlook.com>
2025-07-30 14:32:50 +0100

    Create test to fetch building data including roof info and save to json

--- src/os_api.py
+++ src/os_api.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlencode
-from src.variables import OS_BASE_URL, OS_KEY
+from src.variables import OS_BASE_URL, OS_KEY, OS_ROOF_URL
 import requests
 
 
@@ -17,6 +17,15 @@ def os_places_api_call(uprn):
     endpoint_url = f"https://api.os.uk/search/places/v1/uprn?uprn={uprn}&key={OS_KEY}"
     try:
         response = requests.get(endpoint_url)
+        data = response.json()
+        return data
+    except Exception:
+        return False
+
+def os_roof_api_call(headers, params):
+    full_url = f"{OS_ROOF_URL}{urlencode(params)}"
+    try:
+        response = requests.get(full_url, headers=headers)
         data = response.json()
         return data
     except Exception:
