0bd4ae44f965011dc99d765176c6f1c2035da9e2
Adil Rahman <arahman.99@outlook.com>
2025-07-30 16:32:28 +0100

    Update os dummy data with v4 data

--- src/os_api.py
+++ src/os_api.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlencode
-from src.variables import OS_BASE_URL, OS_KEY, OS_ROOF_URL
+from src.variables import OS_BASE_URL, OS_KEY
 import requests
 
 
@@ -17,15 +17,6 @@ def os_places_api_call(uprn):
     endpoint_url = f"https://api.os.uk/search/places/v1/uprn?uprn={uprn}&key={OS_KEY}"
     try:
         response = requests.get(endpoint_url)
-        data = response.json()
-        return data
-    except Exception:
-        return False
-
-def os_roof_api_call(headers, params):
-    full_url = f"{OS_ROOF_URL}{urlencode(params)}"
-    try:
-        response = requests.get(full_url, headers=headers)
         data = response.json()
         return data
     except Exception:
