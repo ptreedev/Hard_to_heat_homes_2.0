e2dd95ff60c354e06c2d03f6dc53bf0126bc8b61
Adil Rahman <arahman.99@outlook.com>
2025-07-30 14:32:50 +0100

    Create test to fetch building data including roof info and save to json

--- src/variables.py
+++ src/variables.py
@@ -7,4 +7,5 @@ OS_KEY = os.getenv("OS_API_KEY")
 
 OS_BASE_URL = "https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-building-2/items?"
 EPC_BASE_URL = 'https://epc.opendatacommunities.org/api/v1/domestic/search?'
+OS_ROOF_URL = "https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-building-4/items?"
 
