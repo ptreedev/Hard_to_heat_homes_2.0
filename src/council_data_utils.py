560a22ad5bfd21c5a0ee528d78e7d9addd5a881d
padlichan <lili.berenyi@gmail.com>
2025-08-06 16:49:47 +0100

    Add polygon data to councils_data and add functionality to retrieve polygon data for a council

--- src/council_data_utils.py
+++ src/council_data_utils.py
@@ -1,21 +1,24 @@
 import geopandas as gpd
 import pandas as pd
 import json
+from shapely.geometry import shape
 
-with open("data/councils_bbox_data.json") as bbox_json:
-        bbox_data = json.load(bbox_json)
-df = pd.read_csv("data/uprn_to_council_data_SE.csv", dtype={"UPRN": str, "COUNCIL_CODE": str})
+with open("data/councils_data_DEMO.json") as bbox_json:
+        councils_data = json.load(bbox_json)
+df = pd.read_csv("data/uprn_to_council_data_SE_DEMO.csv", dtype={"UPRN": str, "COUNCIL_CODE": str})
 
 #use dictionary for speed
 uprn_to_council_dict = pd.Series(df.COUNCIL_CODE.values, index=df.UPRN).to_dict()
 
-def _create_bbox_data():
+def _create_councils_data():
     gdf = gpd.read_file("data/bdline_gpkg_gb/Data/bdline_gb.gpkg", layer = "district_borough_unitary")
 
     #convert coordinates to correct format
     gdf = gdf.to_crs("EPSG:4326")
+    target_council_codes = {"E07000207","E07000116","E07000085"}
+    gdf = gdf[gdf["Census_Code"].isin(target_council_codes)]
 
-    bbox_data = []
+    councils_data = []
     for _, row in gdf.iterrows():
           
         name = row["Name"]
@@ -27,18 +30,20 @@ def _create_bbox_data():
                 "maxx": bounds[2],
                 "maxy": bounds[3]
         }
+        geometry = row.geometry.__geo_interface__
 
-        bbox_data.append({
+        councils_data.append({
             "name": name,
             "census_code": census_code,
-            "bbox": bbox
+            "bbox": bbox,
+            "geometry": geometry
         })
     
-    with open("data/bdline_gpkg_gb/councils_bbox_data.json", "w") as f:
-        json.dump(bbox_data, f, indent=2)
+    with open("data/councils_bbox_data_DEMO.json", "w") as f:
+        json.dump(councils_data, f, indent=2)
 
 def get_bbox_for_council_code(council_code):
-    match = next ((entry for entry in bbox_data if entry["census_code"] == council_code),None)
+    match = next ((entry for entry in councils_data if entry["census_code"] == council_code),None)
     if match:
         bbox_dict = match["bbox"]
         bbox_string = f"{bbox_dict['minx']},{bbox_dict['miny']},{bbox_dict['maxx']},{bbox_dict['maxy']}"
@@ -61,3 +66,18 @@ def get_council_code_for_uprn(uprn):
 def filter_properties_by_council_code(council_code, properties):
     return list(filter(lambda x: get_council_code_for_uprn(str(x.uprn)) == council_code, properties))
 
+def get_polygon_for_council_code(council_code):
+    council_data = next(c for c in councils_data if c["census_code"] == council_code)
+    
+    geom = shape(council_data["geometry"])
+    polygon = geom.geoms[0]
+    original_coords = list(polygon.exterior.coords)
+    latlon_coords = [[lat,lon]for lon, lat in original_coords]
+    if latlon_coords[0] != latlon_coords[-1]:
+        latlon_coords.append(latlon_coords[0])
+
+    request_body = {
+            "type": "Polygon",
+            "coordinates": [latlon_coords]
+        }
+    return request_body
